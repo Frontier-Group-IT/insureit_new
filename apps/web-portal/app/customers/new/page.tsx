92d4376727edf482814c6e6c780e186826807cca
antnish1 <shahdolho@gmail.com>
2026-07-15 13:37:18 +0530

    Pass group options to all child partner onboarding forms

--- apps/web-portal/app/customers/new/page.tsx
+++ apps/web-portal/app/customers/new/page.tsx
@@ -41,7 +41,7 @@ export default async function NewCustomerPage({ searchParams }: { searchParams: 
 
     return (
       <AppShell title={`Add ${dealershipType.toUpperCase()} Dealership`}>
-        <DealershipOnboardingForm action={createDealershipOnboarding} dealershipType={dealershipType} oems={oems} />
+        <DealershipOnboardingForm action={createDealershipOnboarding} dealershipType={dealershipType} oems={oems} groups={groupOptions} />
       </AppShell>
     );
   }
@@ -64,7 +64,7 @@ export default async function NewCustomerPage({ searchParams }: { searchParams: 
 
   return (
     <AppShell title="Add New Customer">
-      <CustomerOnboardingForm action={createCustomerOnboarding} partnerType={partnerType} />
+      <CustomerOnboardingForm action={createCustomerOnboarding} partnerType={partnerType} groups={groupOptions} />
     </AppShell>
   );
 }
