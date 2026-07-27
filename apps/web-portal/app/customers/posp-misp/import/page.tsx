8bf755eba6f3a81979446e74c43f3ff59686cc68
antnish1 <shahdolho@gmail.com>
2026-07-27 23:24:17 +0530

    Use v2 POSP MISP bulk workbook parser

--- apps/web-portal/app/customers/posp-misp/import/page.tsx
+++ apps/web-portal/app/customers/posp-misp/import/page.tsx
@@ -1,6 +1,6 @@
 import { AppShell } from "@/components/shell";
 import { requirePospMispManager } from "@/lib/master-data-server";
-import { uploadPospMispWorkbook } from "../actions";
+import { uploadPospMispWorkbookV2 } from "./bulk-upload-v2-actions";
 import { ImportWorkbookForm } from "./import-workbook-form";
 
 export const dynamic = "force-dynamic";
@@ -10,7 +10,7 @@ export default async function PospMispImportPage() {
   await requirePospMispManager();
   return (
     <AppShell title="Import POSP / MISP">
-      <ImportWorkbookForm action={uploadPospMispWorkbook} />
+      <ImportWorkbookForm action={uploadPospMispWorkbookV2} />
     </AppShell>
   );
 }
