fbc869c529525f1ea30eea9757296cd194f275cb
antnish1 <shahdolho@gmail.com>
2026-07-29 14:03:29 +0530

    Use direct POSP MISP submit action

--- apps/web-portal/app/customers/new/page.tsx
+++ apps/web-portal/app/customers/new/page.tsx
@@ -11,7 +11,7 @@ import { createDealershipOnboarding } from "../dealership-actions";
 import { DealershipOnboardingForm } from "../dealership-onboarding-form";
 import { createGroupOnboarding } from "../group-actions";
 import { GroupOnboardingForm } from "../group-onboarding-form";
-import { createScopedManualPospMispOnboarding } from "../posp-misp/scoped-manual-action";
+import { submitScopedManualPospMispOnboarding } from "../posp-misp/scoped-manual-action";
 import { PospMispOnboardingForm } from "../posp-misp/posp-misp-onboarding-form";
 
 const supportedPartnerTypes = new Set(["individual_proprietor", "dealership", "corporate", "group", "posp", "misp"]);
@@ -75,7 +75,7 @@ export default async function NewCustomerPage({ searchParams }: { searchParams: 
     return (
       <AppShell title={`Add ${partnerType.toUpperCase()} Application`}>
         <PospMispOnboardingForm
-          action={createScopedManualPospMispOnboarding}
+          action={submitScopedManualPospMispOnboarding}
           partnerType={partnerType}
           salesManagers={salesManagers}
           oems={oems}
