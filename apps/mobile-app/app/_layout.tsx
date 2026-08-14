de4f1649ee901c04688d4f9f1d5157ec01b61ab9
antnish1 <shahdolho@gmail.com>
2026-08-14 12:09:39 +0530

    Remove blocking app update manager

--- apps/mobile-app/app/_layout.tsx
+++ apps/mobile-app/app/_layout.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { AppLoadingProvider } from '@/components/app-loading';
-import { AppUpdateManager } from '@/components/app-update-manager';
 import { SplashIntro } from '@/components/first-look';
 import { RealtimeNotificationProvider } from '@/components/realtime-notifications';
 
@@ -27,7 +26,6 @@ function RootApplication() {
     <>
       <StatusBar style={introVisible ? 'light' : 'dark'} />
       <RealtimeNotificationProvider>
-        <AppUpdateManager />
         <Stack
           screenOptions={{ headerShown: false, animation: 'none' }}
         />
