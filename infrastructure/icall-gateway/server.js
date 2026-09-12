30d8acfdf93325ad81f27bfd8e7ca8ee94fe6543
antnish1 <shahdolho@gmail.com>
2026-09-12 13:25:05 +0530

    Align gateway trust proxy cleanup

--- infrastructure/icall-gateway/server.js
+++ infrastructure/icall-gateway/server.js
@@ -20,6 +20,7 @@ if (!RELAY_SECRET || !ICALL_BASE_URL || !ICALL_TOKEN) {
 }
 
 app.disable("x-powered-by");
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(express.json({ limit: "100kb" }));
 app.use(rateLimit({ windowMs: 60 * 1000, limit: 60, standardHeaders: true, legacyHeaders: false }));
