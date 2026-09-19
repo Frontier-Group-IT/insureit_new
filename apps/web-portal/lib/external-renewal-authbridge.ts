5ad9c1c5dd9992ce5e25b9e3310e686e14e76c5b
antnish1 <shahdolho@gmail.com>
2026-09-19 13:01:00 +0530

    Normalize full available AuthBridge RC detail set for voice prospects

--- apps/web-portal/lib/external-renewal-authbridge.ts
+++ apps/web-portal/lib/external-renewal-authbridge.ts
@@ -9,23 +9,65 @@ const EXTERNAL_MAPPER_VERSION = "external-renewal-2026-09-19-v2";
 export type ExternalRenewalRcDetails = {
   registrationNumber: string;
   registrationDate: string | null;
+  rto: string | null;
+  rcStatus: string | null;
+  rcStatusAsOn: string | null;
+  ownerName: string | null;
+  ownerSerialNumber: string | null;
+  fatherHusbandName: string | null;
+  permanentAddress: string | null;
+  permanentAddressCity: string | null;
+  permanentAddressDistrict: string | null;
+  permanentAddressState: string | null;
+  permanentAddressPincode: string | null;
+  permanentAddressCountry: string | null;
+  presentAddress: string | null;
+  presentAddressCity: string | null;
+  presentAddressDistrict: string | null;
+  presentAddressState: string | null;
+  presentAddressPincode: string | null;
+  presentAddressCountry: string | null;
   manufacturer: string | null;
   model: string | null;
+  manufactureDate: string | null;
   manufacturingYear: string | null;
   vehicleClass: string | null;
+  vehicleCategory: string | null;
+  bodyType: string | null;
+  color: string | null;
   fuelType: string | null;
+  normsType: string | null;
+  engineNumber: string | null;
   engineCapacityCc: string | null;
+  cylinderCount: string | null;
   seatingCapacity: string | null;
+  standingCapacity: string | null;
+  sleeperCapacity: string | null;
+  wheelBaseMm: string | null;
   gvwKg: string | null;
+  unladenWeightKg: string | null;
+  commercial: string | null;
   chassisNumber: string | null;
   fitnessExpiryDate: string | null;
-  pucExpiryDate: string | null;
   roadTaxExpiryDate: string | null;
-  nationalPermitExpiryDate: string | null;
+  vehicleTaxUptoDate: string | null;
+  pucNumber: string | null;
+  pucExpiryDate: string | null;
+  permitNumber: string | null;
+  permitType: string | null;
+  permitIssueDate: string | null;
+  permitValidFrom: string | null;
   localPermitExpiryDate: string | null;
+  nationalPermitNumber: string | null;
+  nationalPermitIssuedBy: string | null;
+  nationalPermitExpiryDate: string | null;
+  financed: string | null;
+  financerName: string | null;
   insuranceCompany: string | null;
   policyNumber: string | null;
   policyExpiryDate: string | null;
+  blacklistStatus: string | null;
+  nocDetails: string | null;
 };
 
 type OpportunityRow = {
@@ -113,85 +155,148 @@ function fromNormalized(raw: unknown, registrationNumber: string): ExternalRenew
   return {
     registrationNumber,
     registrationDate: toIsoDate(value.registrationDate),
+    rto: cleanText(value.rto),
+    rcStatus: cleanText(value.rcStatus, 40),
+    rcStatusAsOn: toIsoDate(value.rcStatusAsOn),
+    ownerName: cleanText(value.ownerName),
+    ownerSerialNumber: cleanText(value.ownerSerialNumber, 20),
+    fatherHusbandName: cleanText(value.fatherHusbandName),
+    permanentAddress: cleanText(value.permanentAddress, 300),
+    permanentAddressCity: cleanText(value.permanentAddressCity, 80),
+    permanentAddressDistrict: cleanText(value.permanentAddressDistrict, 80),
+    permanentAddressState: cleanText(value.permanentAddressState, 80),
+    permanentAddressPincode: cleanText(value.permanentAddressPincode, 20),
+    permanentAddressCountry: cleanText(value.permanentAddressCountry, 80),
+    presentAddress: cleanText(value.presentAddress, 300),
+    presentAddressCity: cleanText(value.presentAddressCity, 80),
+    presentAddressDistrict: cleanText(value.presentAddressDistrict, 80),
+    presentAddressState: cleanText(value.presentAddressState, 80),
+    presentAddressPincode: cleanText(value.presentAddressPincode, 20),
+    presentAddressCountry: cleanText(value.presentAddressCountry, 80),
     manufacturer: cleanText(value.manufacturer),
     model: cleanText(value.model),
+    manufactureDate: cleanText(value.manufactureDate, 20),
     manufacturingYear: cleanText(value.manufacturingYear, 4),
-    vehicleClass: cleanText(value.vehicleClass, 40),
+    vehicleClass: cleanText(value.vehicleClass, 80),
+    vehicleCategory: cleanText(value.vehicleCategory, 40),
+    bodyType: cleanText(value.bodyType, 80),
+    color: cleanText(value.color, 40),
     fuelType: cleanText(value.fuelType, 40),
+    normsType: cleanText(value.normsType, 80),
+    engineNumber: cleanCode(value.engineNumber),
     engineCapacityCc: cleanText(value.engineCapacityCc, 40),
+    cylinderCount: cleanText(value.cylinderCount, 20),
     seatingCapacity: cleanText(value.seatingCapacity, 20),
+    standingCapacity: cleanText(value.standingCapacity, 20),
+    sleeperCapacity: cleanText(value.sleeperCapacity, 20),
+    wheelBaseMm: cleanText(value.wheelBaseMm, 40),
     gvwKg: cleanText(value.gvwKg, 40),
+    unladenWeightKg: cleanText(value.unladenWeightKg, 40),
+    commercial: cleanText(value.commercial, 20),
     chassisNumber: cleanCode(value.chassisNumber),
     fitnessExpiryDate: toIsoDate(value.fitnessExpiryDate),
-    pucExpiryDate: toIsoDate(value.pucExpiryDate),
     roadTaxExpiryDate: toIsoDate(value.roadTaxExpiryDate),
-    nationalPermitExpiryDate: toIsoDate(value.nationalPermitExpiryDate),
+    vehicleTaxUptoDate: toIsoDate(value.vehicleTaxUptoDate),
+    pucNumber: cleanText(value.pucNumber, 80),
+    pucExpiryDate: toIsoDate(value.pucExpiryDate),
+    permitNumber: cleanText(value.permitNumber, 80),
+    permitType: cleanText(value.permitType, 80),
+    permitIssueDate: toIsoDate(value.permitIssueDate),
+    permitValidFrom: toIsoDate(value.permitValidFrom),
     localPermitExpiryDate: toIsoDate(value.localPermitExpiryDate),
+    nationalPermitNumber: cleanText(value.nationalPermitNumber, 80),
+    nationalPermitIssuedBy: cleanText(value.nationalPermitIssuedBy, 80),
+    nationalPermitExpiryDate: toIsoDate(value.nationalPermitExpiryDate),
+    financed: cleanText(value.financed, 20),
+    financerName: cleanText(value.financerName),
     insuranceCompany: cleanText(value.insuranceCompany),
     policyNumber: cleanPolicyNumber(value.policyNumber),
     policyExpiryDate: toIsoDate(value.policyExpiryDate),
+    blacklistStatus: cleanText(value.blacklistStatus, 80),
+    nocDetails: cleanText(value.nocDetails, 80),
   };
 }
-
 function fromRaw(raw: unknown, registrationNumber: string): ExternalRenewalRcDetails {
   const values = flatten(raw);
   return {
     registrationNumber,
     registrationDate: toIsoDate(pick(values, ["registrationdate","regdate","dateofregistration"])),
+    rto: cleanText(pick(values, ["rto","rtoname"])),
+    rcStatus: cleanText(pick(values, ["status","rcstatus"]), 40),
+    rcStatusAsOn: toIsoDate(pick(values, ["statusason","rcstatusason"])),
+    ownerName: cleanText(pick(values, ["ownersname","ownername"])),
+    ownerSerialNumber: cleanText(pick(values, ["ownersnumber","ownerserialnumber","ownerserialno"]), 20),
+    fatherHusbandName: cleanText(pick(values, ["fathernamehusbandname","fatherhusbandname"])),
+    permanentAddress: cleanText(pick(values, ["permanentaddress","splitpermanantaddress","permanantaddress"]), 300),
+    permanentAddressCity: cleanText(pick(values, ["permanantaddresscity","permanentaddresscity"]), 80),
+    permanentAddressDistrict: cleanText(pick(values, ["permanantaddressdistrict","permanentaddressdistrict"]), 80),
+    permanentAddressState: cleanText(pick(values, ["permanantaddressstate","permanentaddressstate"]), 80),
+    permanentAddressPincode: cleanText(pick(values, ["permanantaddresspincode","permanentaddresspincode"]), 20),
+    permanentAddressCountry: cleanText(pick(values, ["permanantaddresscountry","permanentaddresscountry"]), 80),
+    presentAddress: cleanText(pick(values, ["presentaddress","splitpresentaddress"]), 300),
+    presentAddressCity: cleanText(pick(values, ["presentaddresscity"]), 80),
+    presentAddressDistrict: cleanText(pick(values, ["presentaddressdistrict"]), 80),
+    presentAddressState: cleanText(pick(values, ["presentaddressstate"]), 80),
+    presentAddressPincode: cleanText(pick(values, ["presentaddresspincode"]), 20),
+    presentAddressCountry: cleanText(pick(values, ["presentaddresscountry"]), 80),
     manufacturer: cleanText(pick(values, ["makermanufacturer","manufacturer","maker","vehiclemanufacturer","vehiclemaker"])),
     model: cleanText(pick(values, ["modelmakersclass","model","modelname","vehiclemodel","variant"])),
+    manufactureDate: cleanText(pick(values, ["manufacturedate","manufacturingdate"]), 20),
     manufacturingYear: cleanText(pick(values, ["manufacturingyear","manufactureyear","mfgyear","yearofmanufacture"]), 4),
-    vehicleClass: cleanText(pick(values, ["vehicleclass","vehicleclassdesc","classofvehicle","vehiclecategory","vehicletype","bodytype"]), 40),
+    vehicleClass: cleanText(pick(values, ["vehicleclass","vehicleclassdesc","classofvehicle","vehicletype"]), 80),
+    vehicleCategory: cleanText(pick(values, ["vehiclecategory"]), 40),
+    bodyType: cleanText(pick(values, ["bodytype"]), 80),
+    color: cleanText(pick(values, ["color","colour"]), 40),
     fuelType: cleanText(pick(values, ["fueltype","fuel","fueldescription"]), 40),
-    engineCapacityCc: cleanText(pick(values, ["cubiccapacity","cubiccapacitycc","enginecapacity","enginecapacitycc","enginecc","cc"]), 40),
+    normsType: cleanText(pick(values, ["normstype","emissionnorms","bharatstage"]), 80),
+    engineNumber: cleanCode(pick(values, ["enginenumber","engineno"])),
+    engineCapacityCc: cleanText(pick(values, ["enginecapacity","enginecapacitycc","cubiccapacity","cubiccapacitycc","enginecc","cc"]), 40),
+    cylinderCount: cleanText(pick(values, ["noofcylinder","numberofcylinders","cylindercount"]), 20),
     seatingCapacity: cleanText(pick(values, ["seatingcapacity","seatcapacity","numberofseats","totalseats"]), 20),
-    gvwKg: cleanText(pick(values, ["gvw","gvwkg","grossvehicleweight","grossweight"]), 40),
+    standingCapacity: cleanText(pick(values, ["vehiclestandingcapacity","standingcapacity"]), 20),
+    sleeperCapacity: cleanText(pick(values, ["sleepercapacity"]), 20),
+    wheelBaseMm: cleanText(pick(values, ["wheelbase"]), 40),
+    gvwKg: cleanText(pick(values, ["grossweight","gvw","gvwkg","grossvehicleweight"]), 40),
+    unladenWeightKg: cleanText(pick(values, ["unloadingweight","unladenweight","kerbweight"]), 40),
+    commercial: cleanText(pick(values, ["iscommercial","commercial"]), 20),
     chassisNumber: cleanCode(pick(values, ["chassisnumber","chassisno","chassis"])),
-    fitnessExpiryDate: toIsoDate(pick(values, ["fitnessexpirydate","fitnessupto","fitnessvalidupto"])),
-    pucExpiryDate: toIsoDate(pick(values, ["pucexpirydate","puccupto","pucupto","pucvalidupto","pollutionupto"])),
-    roadTaxExpiryDate: toIsoDate(pick(values, ["roadtaxexpirydate","taxupto","taxvalidupto","roadtaxupto"])),
-    nationalPermitExpiryDate: toIsoDate(pick(values, ["nationalpermitexpirydate","nationalpermitupto","nationalpermitvalidupto"])),
-    localPermitExpiryDate: toIsoDate(pick(values, ["localpermitexpirydate","localpermitupto","localpermitvalidupto","permitupto","permitvalidupto"])),
+    fitnessExpiryDate: toIsoDate(pick(values, ["fitnessdatercexpirydate","fitnessexpirydate","fitnessupto","fitnessvalidupto"])),
+    roadTaxExpiryDate: toIsoDate(pick(values, ["taxupto","roadtaxexpirydate","taxvalidupto","roadtaxupto"])),
+    vehicleTaxUptoDate: toIsoDate(pick(values, ["vehicletaxupto"])),
+    pucNumber: cleanText(pick(values, ["puccno","pucnumber"]), 80),
+    pucExpiryDate: toIsoDate(pick(values, ["puccupto","pucexpirydate","pucupto","pucvalidupto","pollutionupto"])),
+    permitNumber: cleanText(pick(values, ["permitnumber"]), 80),
+    permitType: cleanText(pick(values, ["permittype"]), 80),
+    permitIssueDate: toIsoDate(pick(values, ["permitissuedate"])),
+    permitValidFrom: toIsoDate(pick(values, ["permitvaldfrom","permitvalidfrom"])),
+    localPermitExpiryDate: toIsoDate(pick(values, ["permitvalidupto","localpermitexpirydate","localpermitupto","localpermitvalidupto"])),
+    nationalPermitNumber: cleanText(pick(values, ["nationalpermitnumber"]), 80),
+    nationalPermitIssuedBy: cleanText(pick(values, ["nationalpermitissuedby"]), 80),
+    nationalPermitExpiryDate: toIsoDate(pick(values, ["nationalpermitupto","nationalpermitexpirydate","nationalpermitvalidupto"])),
+    financed: cleanText(pick(values, ["financed"]), 20),
+    financerName: cleanText(pick(values, ["financername","financiername"])),
     insuranceCompany: cleanText(pick(values, ["insurancecompany","insurer","insurername"])),
     policyNumber: cleanPolicyNumber(pick(values, ["policynumber","policyno","insurancepolicynumber"])),
     policyExpiryDate: toIsoDate(pick(values, ["insurancetodateinsuranceupto","insurancetodate","insuranceupto","policyexpirydate","insuranceexpirydate"])),
+    blacklistStatus: cleanText(pick(values, ["blackliststatus"]), 80),
+    nocDetails: cleanText(pick(values, ["nocdetails"]), 80),
   };
 }
-
 function mergeDetails(
   preferred: ExternalRenewalRcDetails,
   fallback: ExternalRenewalRcDetails,
 ): ExternalRenewalRcDetails {
-  return {
-    registrationNumber: preferred.registrationNumber || fallback.registrationNumber,
-    registrationDate: preferred.registrationDate ?? fallback.registrationDate,
-    manufacturer: preferred.manufacturer ?? fallback.manufacturer,
-    model: preferred.model ?? fallback.model,
-    manufacturingYear: preferred.manufacturingYear ?? fallback.manufacturingYear,
-    vehicleClass: preferred.vehicleClass ?? fallback.vehicleClass,
-    fuelType: preferred.fuelType ?? fallback.fuelType,
-    engineCapacityCc: preferred.engineCapacityCc ?? fallback.engineCapacityCc,
-    seatingCapacity: preferred.seatingCapacity ?? fallback.seatingCapacity,
-    gvwKg: preferred.gvwKg ?? fallback.gvwKg,
-    chassisNumber: preferred.chassisNumber ?? fallback.chassisNumber,
-    fitnessExpiryDate: preferred.fitnessExpiryDate ?? fallback.fitnessExpiryDate,
-    pucExpiryDate: preferred.pucExpiryDate ?? fallback.pucExpiryDate,
-    roadTaxExpiryDate: preferred.roadTaxExpiryDate ?? fallback.roadTaxExpiryDate,
-    nationalPermitExpiryDate: preferred.nationalPermitExpiryDate ?? fallback.nationalPermitExpiryDate,
-    localPermitExpiryDate: preferred.localPermitExpiryDate ?? fallback.localPermitExpiryDate,
-    insuranceCompany: preferred.insuranceCompany ?? fallback.insuranceCompany,
-    policyNumber: preferred.policyNumber ?? fallback.policyNumber,
-    policyExpiryDate: preferred.policyExpiryDate ?? fallback.policyExpiryDate,
-  };
+  const result = { ...preferred } as Record<string, unknown>;
+  for (const [key, value] of Object.entries(fallback)) {
+    if (result[key] == null || result[key] === "") result[key] = value;
+  }
+  return result as ExternalRenewalRcDetails;
 }
-
 function usefulFieldCount(details: ExternalRenewalRcDetails) {
-  return [
-    details.manufacturer, details.model, details.chassisNumber, details.insuranceCompany, details.policyNumber,
-    details.policyExpiryDate, details.registrationDate, details.manufacturingYear, details.vehicleClass, details.fuelType,
-  ].filter(Boolean).length;
+  return Object.entries(details)
+    .filter(([key, value]) => key !== "registrationNumber" && value != null && value !== "")
+    .length;
 }
-
 async function persistOpportunityResult(
   opportunityId: string,
   status: "ready" | "no_data" | "failed",
